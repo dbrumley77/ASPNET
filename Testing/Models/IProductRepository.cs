cc669e950f17fdfe2a60e60267818e781b914c00
Douglas <douglas-brumley@hotmail.com>
2024-07-12 11:40:17 -0400

    initial commit

---
+++ Testing/Models/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Testing.Models
+{
+    public interface IProductRepository
+    {
+
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+        public void InsertProduct(Product productToInsert);
+        public void DeleteProduct(Product product);
+
+    }
+    
+
+
+}
